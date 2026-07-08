c35064ae8d17721901ffe60e54f2c23d80da10ff
Harsh Bhardwaj <harshbhardwaj94162@gmail.com>
2026-07-08 21:01:15 +0530

    Strings

---
+++ js_strings.js
@@ -0,0 +1,15 @@
+console.log("Strings in Javascript")
+//Strings in JS
+let pers1 = "Harsh Bhardwaj"
+let pers2= "Vinay Sharma"
+let pers3 = "Goldi Sharma"
+console.log(pers1)
+console.log(pers2)
+console.log(pers3)
+
+// strings with quotes
+let ans = "It's raining today"
+console.log(ans)
+
+let len = pers1.length;
+console.log(len)
--- loops.js
+++ loops.js
@@ -1,32 +1,96 @@
 //Loops in javascript
 //For Loop 
 
-for(i=0; i<10; i++){ //simple for loop
-    console.log(i)
+// for(i=4; i<10; i++){ //simple for loop
+//     console.log(i)
+// }
+
+// to print any word many times
+// for(i=0; i<=10; i++){
+//     console.log("How are youuuuuuuuuu")
+// }
+
+// //to print even numbers 
+// for(i=2; i<=20; i+=2){
+//     console.log(i)
+// }
+
+// //to find the sum 
+// let sum =0;
+// for(let i=0; i<100; i++){
+//     sum+=i
+    
+// }
+// console.log(sum)
+
+// // to use for loop in an array
+// const cars = ["BMW","AUDI","MERCEDES","POLO"]
+// let len = cars.length;
+// let text = "";
+// for(i=0; i<len; i++){
+//     console.log(cars[i])
+// }
+
+//while loop
+//a simple while loop
+let i = 1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+let x =10;
+while(x<30){
+    console.log(x+=1)
+    x++;
 }
 
 // to print any word many times
-for(i=0; i<=10; i++){
-    console.log("How are youuuuuuuuuu")
+let y =0;
+while(y<10){
+    console.log("HEYYYYYYYYYYY")
+    y++;
 }
 
-//to print even numbers 
-for(i=2; i<=20; i+=2){
-    console.log(i)
-}
+//to print sum in while loop
 
-//to find the sum 
-let sum =0;
-for(let i=0; i<100; i++){
-    sum+=i
-    
+let z =1;
+let sum = 0;
+while(z<=10){
+    sum+=z;
+    z++;
 }
 console.log(sum)
 
-// to use for loop in an array
-const cars = ["BMW","AUDI","MERCEDES","POLO"]
-let len = cars.length;
-let text = "";
-for(i=0; i<len; i++){
-    console.log(cars[i])
+// do while loop
+let m =2
+do{
+    console.log(m)
+    m++;
 }
+while(m<10)
+
+//print even nukbers in do while loop
+let a = 2;
+do{
+    console.log(a);
+    a+=2;
+}
+while(a<=10)
+
+// use of break in loops
+for(c=0; c<=8;c++){
+    if(c===5){
+        break;
+    }
+    console.log(c)
+}
+
+//continue in loops
+for(h=0;h<=10; h++){
+    if(h==6){
+        continue;
+    }
+    console.log(h)
+}
+
