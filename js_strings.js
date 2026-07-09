59109081bc4ca7cc87ffef69a52e1e7554bc5434
Harsh Bhardwaj <harshbhardwaj94162@gmail.com>
2026-07-09 17:42:00 +0530

    String methods

--- js_strings.js
+++ js_strings.js
@@ -11,5 +11,31 @@ console.log(pers3)
 let ans = "It's raining today"
 console.log(ans)
 
+//length of string
 let len = pers1.length;
 console.log(len)
+
+//escape chracter sequence
+let text = "Where are \"you\" going ?" //double quotes
+console.log(text)
+
+let text2 = 'Where are\' you Mr. Mehta ?' //single  quotes
+console.log(text2)
+
+let text3 = "This example \\ is for backslash"
+console.log(text3)
+
+
+// template strings allows single and double quotes using backtics
+let tmpstr = `This is 'example of template "string`
+console.log(tmpstr)
+
+//using symbol in string
+let symstr = 'This is an ex$$ample of }using symbol{'
+console.log(symstr)
+
+//Various Strings Methods
+let strmethod = "This is an example"
+console.log(strmethod.charAt(5)) //char at position
+console.log(strmethod.charCodeAt(2))
+console.log(strmethod.codePointAt(1))
